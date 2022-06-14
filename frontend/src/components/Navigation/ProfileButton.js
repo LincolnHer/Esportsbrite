51143c85b95b877667657346cf6c40cdd572b602
LincolnHer <lincolnher15@gmail.com>
2022-06-14 11:36:07 -0500

    finished logout css

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
+import { NavLink } from "react-router-dom";
 import * as sessionActions from '../../store/session';
 import "./Navigation.css"
 
@@ -31,19 +32,24 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <div className="splash-link-profile"
+        onMouseEnter={() => setShowMenu(true)}
+        onMouseLeave={() => setShowMenu(false)}
+      >
         <i className="fas fa-user-circle" />
         <span className="profile-email">{user.email}</span>
-      </button>
       {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
+        <div className="profile-dropdown"
+        >
+          <div className="menu">
+            <NavLink to="/events/all">
+              Manage my events
+            </NavLink>
+          </div>
+          <div onClick={logout} className="menu">Log Out</div>
+        </div>
       )}
+      </div>
     </>
   );
 }
