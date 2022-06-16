741cc2685fd71354ad9bedbc25640f1af5e1d9f6
LincolnHer <lincolnher15@gmail.com>
2022-06-16 16:24:21 -0500

    delete crud done

--- frontend/src/components/EventPage/index.js
+++ frontend/src/components/EventPage/index.js
@@ -1,21 +1,33 @@
-import { useSelector } from "react-redux";
-import { NavLink, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useHistory, useParams } from "react-router-dom";
 import Navigation from "../Navigation";
+import { deleteEventThunk } from "../../store/events";
 import "./EventPage.css"
 
 const EventPage = (isLoaded) => {
     const { eventId } = useParams()
+    const dispatch = useDispatch()
+    const history = useHistory()
     const users = useSelector((state) => state.users)
     const events = useSelector((state) => state.events)
     const usersArr = Object.values(users)
     const eventsArr = Object.values(events)
     const currEvent = eventsArr.find(event => event?.id === +eventId)
+    console.log(currEvent)
     const host = usersArr?.find(user => user?.id === currEvent?.hostId)
     const newDate = new Date(currEvent?.date)
     const dateStr = newDate?.toDateString()
     // Tue Oct 04 2022
     const month = dateStr.slice(4, 7)
     const day = dateStr.slice(8, 10)
+
+    const deleteEvent = async (e) => {
+        e.preventDefault()
+        const oldEvent = await dispatch(deleteEventThunk(currEvent))
+        history.push("/")
+    }
+
+
 
     return (
         <div className="Splash-Home">
@@ -37,7 +49,7 @@ const EventPage = (isLoaded) => {
                                         edit
                                     </button>
                                 </NavLink>
-                                <button className="event-btn" type="button" onClick>
+                                <button className="event-btn" type="button" onClick={deleteEvent}>
                                     delete
                                 </button>
                             </div>
