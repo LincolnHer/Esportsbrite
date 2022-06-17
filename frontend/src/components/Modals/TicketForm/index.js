6e04c63abeaca61ee2d76286b6686cf3be44304e
LincolnHer <lincolnher15@gmail.com>
2022-06-17 16:01:27 -0500

    finished create

--- frontend/src/components/Modals/TicketForm/index.js
+++ frontend/src/components/Modals/TicketForm/index.js
@@ -1,39 +1,53 @@
-import { useState } from "react"
-import { useSelector } from "react-redux"
-import ".././Modal.css"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import { postTicketThunk } from "../../../store/tickets";
+import ".././Modal.css";
 
 const TicketForm = () => {
-    const ticket = useSelector((state) => state.tickets)
-    const [quantity, setQuantity] = useState(0)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { eventId } = useParams();
+  const user = useSelector((state) => state.session.user);
+  const ticket = useSelector((state) => state.tickets);
+  const [quantity, setQuantity] = useState(0);
 
-    return (
-        <form className="ticket-form">
-            <div className="ticket-form-container">
-                <div className="ticket-label-container">
-                    <label className="ticket-label">
-                        Tickets *
-                        <input
-                        className="ticket-input"
-                        type="number"
-                        min="0"
-                        value={quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
-                        placeholder="0"
-                        />
-                    </label>
-                    <div className="general-admission">General Admission</div>
-                    <div className="ticket-price">
-                        $32.00
-                    </div>
-                </div>
-                <div className="ticket-btn-container">
-                    <button className="event-btn">
-                        Register
-                    </button>
-                </div>
-            </div>
-        </form>
-    )
-}
+  const submit = async (e) => {
+    e.preventDefault();
+    const ticketFormVal = {
+      eventId: eventId,
+      userId: user?.id,
+      quantity: quantity,
+    };
 
-export default TicketForm
+    const newTicket = await dispatch(postTicketThunk(ticketFormVal));
+    history.push("/tickets");
+  };
+
+  return (
+    <form className="ticket-form" onSubmit={submit}>
+      <div className="ticket-form-container">
+        <div className="ticket-label-container">
+          <label className="ticket-label">
+            Tickets *
+            <input
+              className="ticket-input"
+              type="number"
+              min="0"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+              placeholder="0"
+            />
+          </label>
+          <div className="general-admission">General Admission</div>
+          <div className="ticket-price">$32.00</div>
+        </div>
+        <div className="ticket-btn-container">
+          <button className="event-btn">Register</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default TicketForm;
